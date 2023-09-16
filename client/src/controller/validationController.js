1b3d74ff95676c117e37882b78bc741f503cde9d
Alessio <alessio.englert@bluewin.ch>
2023-09-16 17:34:12 +0200

    feat: added validation conroller with product input validation functions

---
+++ client/src/controller/validationController.js
@@ -0,0 +1,51 @@
+const validateLogin = (username, password) => {
+  const errorPassword = document.getElementsByClassName("error-password")[0];
+  const errorUsername = document.getElementsByClassName("error-username")[0];
+  errorPassword.style.display = "none";
+  errorUsername.style.display = "none";
+  if (username == "" && password == "") {
+    errorPassword.style.display = "block";
+    errorUsername.style.display = "block";
+    return false;
+  }
+
+  if (username == "") {
+    errorUsername.style.display = "block";
+    return false;
+  }
+
+  if (password == "") {
+    errorPassword.style.display = "block";
+    return false;
+  }
+  return true;
+};
+
+const validateProductInput = (sku, name, price, stock) => {
+  let isError = false;
+  if (sku === "") {
+    const errorMessage = document.getElementsByClassName("error-sku")[0];
+    errorMessage.style.display = "block";
+    isError = true;
+  }
+
+  if (name === "") {
+    const errorMessage = document.getElementsByClassName("error-name")[0];
+    errorMessage.style.display = "block";
+    isError = true;
+  }
+
+  if (price === "") {
+    const errorMessage = document.getElementsByClassName("error-price")[0];
+    errorMessage.style.display = "block";
+    isError = true;
+  }
+
+  if (stock === "") {
+    const errorMessage = document.getElementsByClassName("error-stock")[0];
+    errorMessage.style.display = "block";
+    isError = true;
+  }
+
+  return isError;
+};

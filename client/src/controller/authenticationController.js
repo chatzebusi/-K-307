4a65444bc0351bd412d51f6b6c41fcf31c739990
Alessio <alessio.englert@bluewin.ch>
2023-09-16 17:30:31 +0200

    feat: added authentication functions

--- client/src/controller/authenticationController.js
+++ client/src/controller/authenticationController.js
@@ -1,20 +1,47 @@
 const checkCookie = () => {
   const token = getCookie("token");
   if (token) {
+    addNavigationButtons();
+    useGetCategories();
     return;
   }
+
   fetch("../view/login.html")
     .then((response) => response.text())
     .then((data) => {
-      const dashboard = document.getElementById("dashboard");
-      dashboard.innerHTML = data;
+      const popUp = document.getElementById("pop-up");
+      popUp.innerHTML = data;
     });
 };
 
-const login = (event) => {
-  const username = document.getElementById("username");
-  const password = document.getElementById("password");
-  // TODO send this to the server to authenticate
+const getToken = (response) => {
+  if (response.target.status !== 200) {
+    const errorWrong = document.getElementsByClassName("error-wrong")[0];
+    errorWrong.style.display = "block";
+    return;
+  }
+
+  const login = document.getElementById("login");
+  login.style.display = "none";
+
+  addNavigationButtons();
+
+  useGetCategories();
+};
+
+const login = () => {
+  /* const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value; */
+
+  //! remove this lines
+  const username = "root";
+  const password = "sUP3R53CR3T#";
+
+  if (!validateLogin(username, password)) {
+    return;
+  }
+
+  useAuthenticate(username, password);
 };
 
 const getCookie = (name) => {
@@ -26,4 +53,10 @@ const getCookie = (name) => {
   }
 };
 
-window.onload = checkCookie();
+const getCategories = (response) => {
+  useGetProducts(JSON.parse(response.target.response));
+};
+
+const onMounted = () => {
+  checkCookie();
+};

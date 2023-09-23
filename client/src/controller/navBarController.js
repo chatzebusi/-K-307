02d1a2c31f86d162d1b525f837cda5651af2f55a
Alessio <alessio.englert@bluewin.ch>
2023-09-23 07:43:42 +0200

    feat: added navbar button functions

--- client/src/controller/navBarController.js
+++ client/src/controller/navBarController.js
@@ -1,34 +1,21 @@
 const addNavigationButtons = () => {
-  const logoutBtn = document.getElementsByClassName("btn-logout")[0];
-  logoutBtn.style.display = "block";
+  const showCategories = document.getElementsByClassName("btn-logout")[0];
+  showCategories.style.display = "block";
   const productGroupBtn =
     document.getElementsByClassName("btn-productgroup")[0];
   productGroupBtn.style.display = "block";
   const addProductBtn = document.getElementsByClassName("btn-add-product")[0];
   addProductBtn.style.display = "block";
 
-  addNavbarBtnEventListener(logoutBtn, productGroupBtn, addProductBtn);
+  addNavbarBtnEventListener(showCategories, productGroupBtn, addProductBtn);
 };
 
 const addNavbarBtnEventListener = (
-  logoutBtn,
+  showCategories,
   productGroupBtn,
   addProductBtn
 ) => {
-  logoutBtn.addEventListener("click", () => {
-    console.log("button is clicked");
-  });
-  productGroupBtn.addEventListener("click", () => {
-    console.log("button is clicked");
-  });
+  showCategories.addEventListener("click", () => openShowAllCategoriesPopUp());
+  productGroupBtn.addEventListener("click", () => openAddCategoryPopUp());
   addProductBtn.addEventListener("click", () => openAddProductPopUp());
-};
-
-const openAddProductPopUp = () => {
-  fetch("../view/addProduct.html")
-    .then((response) => response.text())
-    .then((data) => {
-      const popUp = document.getElementById("pop-up");
-      popUp.innerHTML = data;
-    });
 };
